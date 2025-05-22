25aaeae60b483007e653d0f72c5db4d7d91086c3
haya raham asif <haya.raham-asif@stu.ocsb.ca>
2025-05-22 20:50:20 +0000

    attempt #2 for battery

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener("scroll", function () {
     document.documentElement.clientHeight;
   let userScrollPos = window.scrollY;
   let batteryWidth = parseInt(window.getComputedStyle(document.getElementById("battery"), null).getPropertyValue("width"))
-  let maxWidth = ((batteryWidth/document.documentElement.clientWidth) * 100) - 4.5
+  let maxWidth = (((batteryWidth-65)/document.documentElement.clientWidth) * 100)
   let widthBasedOnScrollPercentage = maxWidth - ((userScrollPos / maxHeight) * maxWidth); // calulcating what fraction of the webpage the user has scrolled thorugh
   document.getElementById(
     "progress-bar"
